6c082ac8f49b874795c3a4872e9f5956f6e7a9c4
Spectra.codes by Drix10 <drixisgod@gmail.com>
2024-08-17 21:16:11 +0530

    generated file: `pages/api/auth/[...]nextauth].js`

---
+++ pages/api/auth/[...]nextauth].js
@@ -0,0 +1,42 @@
+import NextAuth from 'next-auth';
+import CredentialsProvider from 'next-auth/providers/credentials';
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import { prisma } from '@/lib/prisma';
+
+export default NextAuth({
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    CredentialsProvider({
+      name: 'Credentials',
+      credentials: {
+        email: { label: 'Email', type: 'text', placeholder: 'example@email.com' },
+        password: { label: 'Password', type: 'password' },
+      },
+      async authorize(credentials, req) {
+        if (!credentials?.email || !credentials?.password) {
+          return null;
+        }
+
+        const user = await prisma.user.findUnique({
+          where: {
+            email: credentials.email,
+          },
+        });
+
+        if (!user || !(await user.comparePassword(credentials.password))) {
+          return null;
+        }
+
+        return {
+          id: user.id,
+          name: user.email,
+          email: user.email,
+        };
+      },
+    }),
+  ],
+  pages: {
+    signIn: '/login',
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+});
